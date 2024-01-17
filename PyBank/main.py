4a0aa9a54c75da78efb6967685aec61c15d5e72a
Elsie Pierre-Antoine <152744433+31573@users.noreply.github.com>
2024-01-17 18:30:54 -0500

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,28 +6,32 @@ Created on Tue Jan 16 13:52:31 2024
 """
 
 
-import statistics
+# To import csv file
 import csv
-
+# to open file with file path
 with open("Resources\\budget_data.csv", "r") as f:
     reader = csv.reader(f)
 
 # skip header row  
     next(reader)
     
+# To set variables  
     total = 0
     months = 0    
     pre_pl = 0
     total_ch = 0
     max_inc = 0
     max_dec = 0
-   
+    
+# To set up loop   
     for row in reader:
         months += 1
         
+# To loop and sum the profit & Loss        
         p_l = int(row[1])
         total += p_l
-          
+
+# to calculate the monthly variance          
       #Average Change
         ch = p_l - pre_pl
         if pre_pl == 0:
@@ -45,11 +49,11 @@ with open("Resources\\budget_data.csv", "r") as f:
             max_dec = ch
             max_dec_month = row[0]
        
-        
+# to reset the p_l     
         pre_pl = p_l
         
         
-                
+ # use f string template and display the results               
 output = f'''
 Financial Analysis
 ------------------------------------------------------------
